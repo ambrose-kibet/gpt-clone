8b555afc0f412f3ebcb4159a145de4fbb3403d37
ambrose-kibet <ambrosekibet36@gmail.com>
2023-12-21 20:51:04 +0300

    add boilerplate code

--- app/(dashboard)/chat/page.tsx
+++ app/(dashboard)/chat/page.tsx
@@ -1,4 +1,19 @@
+import { IoSend } from 'react-icons/io5';
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  return (
+    <div className="flex flex-col min-h-full">
+      <div className="flex-grow">messages</div>
+      <form className="form join mt-auto min-w-full">
+        <input
+          type="text"
+          className="input input-primary input-bordered join-item w-full"
+          placeholder="Ask GPT a question"
+        />
+        <button className="btn btn-primary join-item text-2xl">
+          <IoSend />
+        </button>
+      </form>
+    </div>
+  );
 };
 export default ChatPage;
