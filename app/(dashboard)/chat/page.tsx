6a3de357f391310200039058df9166d34cc55833
ambrose-kibet <ambrosekibet36@gmail.com>
2023-12-30 11:30:59 +0300

    Refactor chat page component to use Chat component and react-query

--- app/(dashboard)/chat/page.tsx
+++ app/(dashboard)/chat/page.tsx
@@ -1,19 +1,15 @@
-import { IoSend } from 'react-icons/io5';
+import Chat from '@/app/components/Chat';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 const ChatPage = () => {
+  const queryClient = new QueryClient();
   return (
-    <div className="flex flex-col min-h-full">
-      <div className="flex-grow">messages</div>
-      <form className="form join mt-auto min-w-full">
-        <input
-          type="text"
-          className="input input-primary input-bordered join-item w-full"
-          placeholder="Ask GPT a question"
-        />
-        <button className="btn btn-primary join-item text-2xl">
-          <IoSend />
-        </button>
-      </form>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Chat />
+    </HydrationBoundary>
   );
 };
 export default ChatPage;
