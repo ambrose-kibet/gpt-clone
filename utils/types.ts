7d53ebebbc8b0d3f43ee2fe0d9bc08a595a55e89
ambrose-kibet <ambrosekibet36@gmail.com>
2024-01-04 21:52:16 +0300

    Add types for Tour and Destination

---
+++ utils/types.ts
@@ -0,0 +1,11 @@
+export type Tour = {
+  city: string;
+  country: string;
+  title: string;
+  description: string;
+  stops: string[];
+};
+export type Destination = {
+  city: string;
+  country: string;
+};
