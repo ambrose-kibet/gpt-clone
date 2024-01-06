6b3b0db65913d951b92d09178b87df89407144cd
ambrose-kibet <ambrosekibet36@gmail.com>
2024-01-06 12:41:54 +0300

    Add tour image to SingleTourPage

--- app/(dashboard)/tours/[id]/page.tsx
+++ app/(dashboard)/tours/[id]/page.tsx
@@ -1,13 +1,19 @@
 import SingleTour from '@/app/components/SingleTour';
 import { getSingleTour } from '@/utils/actions';
 import { Tour } from '@/utils/types';
+import axios from 'axios';
+import Image from 'next/image';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+const url = `https://api.unsplash.com/search/photos?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}&query=`;
 
 const SingleTourPage = async ({ params }: { params: any }) => {
   const { id } = params;
   const tour = await getSingleTour(id);
+  const { data } = await axios(`${url}${tour?.city}`);
+  const tourImage = data?.results[0]?.urls?.raw;
   if (!tour) return redirect('/tours');
+
   return (
     <div>
       <div className="text-sm breadcrumbs mb-12">
@@ -20,6 +26,14 @@ const SingleTourPage = async ({ params }: { params: any }) => {
           </li>
         </ul>
       </div>
+      <Image
+        src={tourImage}
+        width={300}
+        height={300}
+        alt={tour.city}
+        className="rounded-lg w-96 h-96 shadow-xl object-cover "
+        priority
+      />
 
       <SingleTour tour={tour as unknown as Tour} />
     </div>
