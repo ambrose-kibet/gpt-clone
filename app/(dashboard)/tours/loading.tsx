c7063b4ad207706b9a6e963c6926c70ce040771d
ambrose-kibet <ambrosekibet36@gmail.com>
2024-01-06 12:41:47 +0300

    Refactor loading component styles

--- app/(dashboard)/tours/loading.tsx
+++ app/(dashboard)/tours/loading.tsx
@@ -1,19 +1,19 @@
 const loading = () => {
   return (
-    <div className="min-w-full flex flex-col py-16  items-center">
-      <div className="h-16 rounded-full w-72 skeleton mb-16"></div>
-      <div className="flex gap-4 w-full flex-wrap">
+    <div className="min-w-full flex flex-col ">
+      <div className="h-16 rounded-full w-72 skeleton mb-16 mx-auto"></div>
+      <div className="flex gap-4 w-full ">
         <div className="w-1/4">
-          <div className="h-16 rounded-lg skeleton w-full"></div>
+          <div className="h-16 skeleton w-full  rounded-full"></div>
         </div>
         <div className="w-1/4">
-          <div className="h-16 rounded-lg skeleton w-full"></div>
+          <div className="h-16  skeleton w-full rounded-full"></div>
         </div>
         <div className="w-1/4">
-          <div className="h-16 rounded-lg skeleton w-full"></div>
+          <div className="h-16  skeleton w-full rounded-full"></div>
         </div>
         <div className="w-1/4">
-          <div className="h-16 rounded-lg skeleton w-full"></div>
+          <div className="h-16 skeleton w-full rounded-full"></div>
         </div>
       </div>
     </div>
