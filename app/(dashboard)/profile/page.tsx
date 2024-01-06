8d91e20e96efdfea6e75dd8a3de2f3e87045c397
ambrose-kibet <ambrosekibet36@gmail.com>
2024-01-06 12:41:42 +0300

    Add token count to profile page

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -1,6 +1,17 @@
-import { UserProfile } from '@clerk/nextjs';
+import { fetchOrGenerateTokens } from '@/utils/actions';
+import { UserProfile, auth } from '@clerk/nextjs';
 
-const ProfilePage = () => {
-  return <UserProfile />;
+const ProfilePage = async () => {
+  const { userId } = auth();
+  const tokenCount = await fetchOrGenerateTokens(userId as string);
+
+  return (
+    <div>
+      <h2 className="font-extrabold text-2xl mb-8 ml-8">
+        Token Amount: {tokenCount}
+      </h2>
+      <UserProfile />;
+    </div>
+  );
 };
 export default ProfilePage;
