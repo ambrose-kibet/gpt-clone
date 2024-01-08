c918735e32c1401ab015c64a50fe46846067a2cd
ambrose-kibet <ambrosekibet36@gmail.com>
2024-01-08 23:08:09 +0300

    aad dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import Provider from './provider';
-
+import { dark } from '@clerk/themes';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,7 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
         <body className={inter.className}>
           <Provider>{children}</Provider>
